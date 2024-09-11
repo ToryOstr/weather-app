1663ecfba399911dee63e4c0d9293ee84050dd46
ToryOstr <vik.ostrishko@gmail.com>
2024-09-11 17:15:27 +0300

    fix: try fix logo text position

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -2,25 +2,27 @@ import { motion } from "framer-motion";
 
 import styles from "./Logo.module.css";
 
-import img from './sun.png';
+import img from "./sun.png";
 
-export default function Logo(){
+export default function Logo() {
   return (
     <div className={styles.logoWrapper}>
-      <motion.div
-        style={{
-          width: "60px",
-          height: "60px",
-          position: "relative",
-        }}
-        initial={{ rotate: 0 }}
-        animate={{ rotate: 360 }}
-        transition={{ duration: 8, repeat: Infinity, ease: "linear" }}
-      >
-        <img className={styles.imgLogo} src={img} alt="sun" />
-      </motion.div>
+      <div>
+        <motion.div
+          style={{
+            width: "60px",
+            height: "60px",
+            position: "relative",
+          }}
+          initial={{ rotate: 0 }}
+          animate={{ rotate: 360 }}
+          transition={{ duration: 8, repeat: Infinity, ease: "linear" }}
+        >
+          <img className={styles.imgLogo} src={img} alt="sun" />
+        </motion.div>
+      </div>
+
       <span className={styles.logoTxt}> Your Weather</span>
     </div>
-    
-  )
+  );
 }
